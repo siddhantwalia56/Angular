e55e9be1d908f084662f2a0e40633333cc4a7eaa
Siddhant Walia <siddhantwalia56@gmail.com>
2023-02-01 00:04:59 +0530

    inline template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  template:`<app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
