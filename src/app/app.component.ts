0f6d0f64aa3314ba9d9a5f4f35e51bbc1c07f49c
Siddhant Walia <siddhantwalia56@gmail.com>
2023-02-08 16:18:38 +0530

    Recap ngFor and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // name='Siddhant'
    title = 'first-app';
-  serverElements=[{type:'server', name:'Testserver',content:'Just a test!'}]
+  // serverElements=[{type:'server', name:'Testserver',content:'Just a test!'}]
 
-  onServerAdded(serverData:{serverName:string,serverContent: string}){
-    this.serverElements.push({
-      type:'server',
-      name:serverData.serverName,
-      content:serverData.serverContent
-    })
-  }
+  // onServerAdded(serverData:{serverName:string,serverContent: string}){
+  //   this.serverElements.push({
+  //     type:'server',
+  //     name:serverData.serverName,
+  //     content:serverData.serverContent
+  //   })
+  // }
 
-  onBlueprintAdded(blueprintData:{serverName:string,serverContent: string}){
-    this.serverElements.push({
-      type:'blueprint',
-      name:blueprintData.serverName,
-      content:blueprintData.serverContent
-    })
-  }
-  OnChange(){
-   this.serverElements[0].name='changed';
-  }
+  // onBlueprintAdded(blueprintData:{serverName:string,serverContent: string}){
+  //   this.serverElements.push({
+  //     type:'blueprint',
+  //     name:blueprintData.serverName,
+  //     content:blueprintData.serverContent
+  //   })
+  // }
+  // OnChange(){
+  //  this.serverElements[0].name='changed';
+  // }
+  numbers = [1, 2, 3, 4, 5];
+  oddnumbers=[1,3,5,7]
+  evennumbers=[2,4,6,8]
+  onlyOdd = false;
 }
