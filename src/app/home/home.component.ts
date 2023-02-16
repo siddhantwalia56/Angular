6e0dbbf76b4d3fcdbc7b0af9e28f6b8b5ac09036
Siddhant Walia <siddhantwalia56@gmail.com>
2023-02-16 13:58:10 +0530

    understood relative path

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router'
+import {Router, ActivatedRoute} from '@angular/router'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,12 +7,13 @@ import {Router} from '@angular/router'
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,
+              private route:ActivatedRoute) { }
 
   ngOnInit() {
   }
   onclick(){
     // complex computation
-    this.router.navigate(['/servers'])
+    this.router.navigate(['home'],{relativeTo:this.route})
   }
 }
