545410504271b74eb1960656bc58ce3123afe3f7
Siddhant Walia <siddhantwalia56@gmail.com>
2023-02-21 17:31:11 +0530

    created first custom Observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponentObs implements OnInit{
+export class HomeComponentObs implements OnInit, OnDestroy{
+
+  private firstObsSubscription;
   constructor() { }
 
   ngOnInit() {
+   this.firstObsSubscription= interval(1000).subscribe(count=>{
+      console.log(count)
+    })
+  }
+
+  ngOnDestroy(){
+    this.firstObsSubscription.unsubscribe()
   }
 }
