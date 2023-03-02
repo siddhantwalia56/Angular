0ba59b307f95c1fbc2d802a7b6303371b6036de2
Siddhant Walia <siddhantwalia56@gmail.com>
2023-03-02 12:28:58 +0530

    Analysing testing setup by cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -93,51 +93,51 @@ export class AppComponent {
 // onsaveProject(){
 //   console.log(this.projectForm.value)
 // }
-filteredStatus:string='';
-appstatus=new Promise((resolve,reject)=>{
-  setTimeout(() => {
-    resolve('Stable')
-  }, 2000);
-})
-servers = [
-  {
-    instanceType: 'medium',
-    name: 'Production Server',
-    status: 'stable',
-    started: new Date(15, 1, 2017)
-  },
-  {
-    instanceType: 'large',
-    name: 'User Database',
-    status: 'stable',
-    started: new Date(15, 1, 2017)
-  },
-  {
-    instanceType: 'small',
-    name: 'Development Server',
-    status: 'offline',
-    started: new Date(15, 1, 2017)
-  },
-  {
-    instanceType: 'small',
-    name: 'Testing Environment Server',
-    status: 'stable',
-    started: new Date(15, 1, 2017)
-  }
-];
-getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
-  return {
-    'list-group-item-success': server.status === 'stable',
-    'list-group-item-warning': server.status === 'offline',
-    'list-group-item-danger': server.status === 'critical'
-  };
-}
-OnAdd(){
-  this.servers.push({
-    instanceType: 'small',
-    name: 'New Server',
-    status: 'stable',
-    started: new Date(15, 5, 2023)
-  })
-}
+// filteredStatus:string='';
+// appstatus=new Promise((resolve,reject)=>{
+//   setTimeout(() => {
+//     resolve('Stable')
+//   }, 2000);
+// })
+// servers = [
+//   {
+//     instanceType: 'medium',
+//     name: 'Production Server',
+//     status: 'stable',
+//     started: new Date(15, 1, 2017)
+//   },
+//   {
+//     instanceType: 'large',
+//     name: 'User Database',
+//     status: 'stable',
+//     started: new Date(15, 1, 2017)
+//   },
+//   {
+//     instanceType: 'small',
+//     name: 'Development Server',
+//     status: 'offline',
+//     started: new Date(15, 1, 2017)
+//   },
+//   {
+//     instanceType: 'small',
+//     name: 'Testing Environment Server',
+//     status: 'stable',
+//     started: new Date(15, 1, 2017)
+//   }
+// ];
+// getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+//   return {
+//     'list-group-item-success': server.status === 'stable',
+//     'list-group-item-warning': server.status === 'offline',
+//     'list-group-item-danger': server.status === 'critical'
+//   };
+// }
+// OnAdd(){
+//   this.servers.push({
+//     instanceType: 'small',
+//     name: 'New Server',
+//     status: 'stable',
+//     started: new Date(15, 5, 2023)
+//   })
+// }
 }
